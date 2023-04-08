7e15beb62a0c9c16813ab40d17be57464f6fb050
unknown <sadiqunnur.97@gmail.com>
2023-04-09 05:49:04 +0600

    lesson-4 added

--- JS/AJAX/lesson-4/script.js
+++ JS/AJAX/lesson-4/script.js
@@ -1,16 +1,18 @@
+
 const loadData = function(){
     const xhr = new XMLHttpRequest();
 
     //what to do when response arrives
 
     xhr.onload = function(){   //this function will be called after getting the response
-        console.log('Request Finished')
         const container = document.getElementById('demo');
         container.innerHTML = xhr.responseText;
+
+        console.log(this.getAllResponseHeaders()); //will return all the response headers
     };
 
     //prepare request - methods : GET, POST, PUT, PATCH, DELETE, OPTIONS etc
-    xhr.open("GET", "./data/data.txt",false); //The 3rd parameter defines whether the request sending task will be asynchronous or not. true means synchronous and false means asynchronous
+    xhr.open("GET", "./data/data.txt"); //The 3rd parameter defines whether the request sending task will be asynchronous or not. true means synchronous and false means asynchronous
 
     //send request
     xhr.send();
